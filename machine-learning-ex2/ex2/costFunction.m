8bb0fa1481f6767430631de89f2016a43da20f7e
KerryTang <sunrain81928@gmail.com>
2016-01-25 10:21:33 +0800

    Complete hw2 and initialize with hw3

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -19,10 +19,10 @@ grad = zeros(size(theta));
 %
 % Note: grad should have the same dimensions as theta
 %
+
 h = sigmoid(X * theta);
 
 J = (1/m) * sum(log(h')*(-y) - log( 1 - h')*(1-y));
-
 
 grad = (1/m) * (X' * (h - y));
 
--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -18,7 +18,11 @@ grad = zeros(size(theta));
 %               derivatives of the cost w.r.t. each parameter in theta
 
 
+h = sigmoid(X * theta);
 
+J = (1/m) * sum(log(h')*(-y) - log( 1 - h')*(1-y)) + lambda/(2*m) * sum(theta(2:end) .^2 );
+
+grad = (1/m) * (X' * (h - y)) + lambda/m*[0;theta(2:end)];
 
 
 
